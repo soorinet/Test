b909a6671c56d20163b7ab33105db97dac2e2320
Soojin Park <soorinet0@naver.com>
2020-03-20 16:39:09 +0900

    Initial Commit

---
+++ soyoyou/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  soyoyou
+//
+//  Created by Soojin Park on 2020/03/20.
+//  Copyright © 2020 Soojin Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
